6843ce92a87951e809b34f2e4d8f866a3fa0c6b5
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-07-30 12:15:31 -0500

    add initial Grub superclass prototype code in ES6

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,18 @@
+// class Grub extends React.Component {
+//   constructor {
+//     this.age: 0;
+//   }
+// };
+
 class Grub {
-};
+  //call a constructor
+  constructor() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+  }
+  //add a method
+  eat() {
+    return 'Mmmmmmmmm jelly';
+  }
+}
